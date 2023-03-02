dd71ff375e5cb6aabcc094ecd29e7b5e2604edec
Patrick-A47 <72445293+Patrick-A47@users.noreply.github.com>
2023-03-02 02:10:09 +0000

    Added sliders for drink and disturbance levels
    UI elements for the players current disturbance and drink level.

--- Assets/Game/Scripts/UI/FillStatusBar.cs
+++ Assets/Game/Scripts/UI/FillStatusBar.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class FillStatusBar : MonoBehaviour
 {
     public CrowdLevel crowd;
+    public DrinkLevel drink;
+    public DistrubanceLevel distrubance;
     public Image fillImage;
     private Slider slider;
     // Start is called before the first frame update
@@ -17,7 +19,12 @@ public class FillStatusBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float fillValue = crowd.currentCrowd / crowd.maxCrowd;
-        slider.value = fillValue;
+        float fillValueCrowd = crowd.currentCrowd / crowd.maxCrowd;
+        slider.value = fillValueCrowd;
+        float fillValueDrink = drink.currentDrink / drink.maxDrink;
+        slider.value = fillValueDrink;
+        float fillValueDisturb = distrubance.currentDisturb/ distrubance.maxDisturb;
+        slider.value = fillValueDisturb;
+
     }
 }
