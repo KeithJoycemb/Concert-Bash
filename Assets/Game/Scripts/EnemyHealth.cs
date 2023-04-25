eab3f7721fc86e9d226093e0056e27a0fecaa6f2
VivienBenoy <72444810+VivienBenoy@users.noreply.github.com>
2023-04-25 21:11:25 +0100

    arrangedProj

---
+++ Assets/Game/Scripts/EnemyHealth.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Npc_AI
+{
+    public class EnemyHealth : CharacterStats, IAttackable
+
+    {
+        private Animator animator;
+        
+
+        public GameObject healthBarUI;
+        public Slider slider;
+        public GameObject throwable;
+
+        protected override void Start()
+        {
+            animator = GetComponent<Animator>();
+            base.Start();
+            slider.value = CalculateHealth();
+            throwable = GetComponent<GameObject>();
+        }
+
+        private void Update()
+        {
+            slider.value = CalculateHealth();
+
+            if (GetCurrentHealth().GetValue() < GetMaxHealth().GetValue())
+            {
+                healthBarUI.SetActive(true);
+            }
+            if (GetCurrentHealth().GetValue() <= 0)
+            {
+                // Destroy(gameObject);
+                animator.enabled = false;
+            }
+            if (GetCurrentHealth().GetValue() > GetMaxHealth().GetValue())
+            {
+                GetCurrentHealth().SetValue(GetMaxHealth().GetValue());
+            }
+        }
+
+
+        float CalculateHealth()
+        {
+            return GetCurrentHealth().GetValue() / GetMaxHealth().GetValue();
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("DisableAnimator"))
+            {
+                TakeDamage(100); // Decrease the enemy health when collided with the player
+            }
+        }
+
+       
+
+        void TakeDamage(int damage)
+        {
+            GetCurrentHealth().SetValue(GetCurrentHealth().GetValue() - damage);
+
+            if (GetCurrentHealth().GetValue() <= 0)
+            {
+                animator.enabled = false;
+            }
+        }
+
+        public override void Attacked(GameObject attacker, Attack attack)
+        {
+            TakeDamage(attacker, attack.Damage);
+            if (GetCurrentHealth().GetValue() <= 0)
+            {
+                OnDeath(attacker);
+            }
+        }
+
+        protected override void OnDeath(GameObject attacker)
+        {
+            base.OnDeath(attacker);
+            animator.enabled = false;
+        }
+        void TakeDamage(GameObject attacker, float damage)
+        {
+            if (damage <= 0f) return;
+           // float armor = GetArmor().GetValue();
+            //damage -= armor;
+            damage = Mathf.Clamp(damage, 0f, float.MaxValue);
+            GetCurrentHealth().SetValue(GetCurrentHealth().GetValue() - damage);
+            //OnHealthValueChanged?.Invoke();
+
+
+        }
+    }
+}
