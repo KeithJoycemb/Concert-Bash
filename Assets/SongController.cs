35ce87fecac0e8fbf5cf6ce4a5f136955927e31a
Patrick-A47 <72445293+Patrick-A47@users.noreply.github.com>
2023-04-26 03:39:04 +0100

    Added more songs and ability to skip between songs

--- Assets/SongController.cs
+++ Assets/SongController.cs
@@ -1,16 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SongController : MonoBehaviour
 {
-    private AudioSource speakerAudioSource;
 
+    [Header("List of Tracks")]
+    [SerializeField] private Track[] audioTracks;
+    private int trackIndex;
+    
+
+    [Header("Text UI")]
+    [SerializeField] private Text trackTextUI;
+
+    private AudioSource speakerAudioSource;
     private void Start()
     {
         speakerAudioSource = GetComponent<AudioSource>();
+
+        trackIndex = 0;
+        speakerAudioSource.clip = audioTracks[trackIndex].trackAudioClip;
+
+        trackTextUI.text = audioTracks[trackIndex].name;
     }
 
+    public void SkipForwardButton()
+    {
+        trackIndex++;
+        UpdateTrack(trackIndex);
+
+    }
+
+    public void SkipBackwardButton()
+    {
+        trackIndex--;
+        UpdateTrack(trackIndex);
+    }
+
+    void UpdateTrack(int index)
+    {
+        speakerAudioSource.clip = audioTracks[index].trackAudioClip;
+        trackTextUI.text = audioTracks[index].name;
+    }
     public void PlayAudio()
     {
         speakerAudioSource.Play();
